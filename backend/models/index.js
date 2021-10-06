866bbb78b620ed27f5ad917ef26efa3ec3c7d923
viktor <dangerouzua@gmail.com>
2021-10-06 11:41:22 +0300

    changed db

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,15 +1,9 @@
 const dbConfig = require("../db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("d3h0qdnv96tsra", "pjisvdjipewfdu","0ad2bb2996624383817b2c4ceb8eb2d8bbe3b80fe1863f6d5b57092ff17d9703", {
-    host: "ec2-63-32-7-190.eu-west-1.compute.amazonaws.com",
-    dialect:  "postgres",
-    dialectOptions: {
-        "ssl": {
-            "require": true,
-            "rejectUnauthorized": false
-        }
-    },
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
     operatorsAliases: false,
 
     pool: {
@@ -26,6 +20,5 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.users = require("./user.js")(sequelize, Sequelize);
-db.playlists = require("./playlist.js")(sequelize, Sequelize);
 
 module.exports = db;
